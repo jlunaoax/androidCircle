87c669a1b40bbbc1daebb3ac681904de59f913d6
javier.luna@wgu.edu <javier.luna@wgu.edu>
2023-06-01 21:52:56 -0600

    Working with RecyclerView

--- app/src/main/java/com/example/androiccircle/MainActivity2.kt
+++ app/src/main/java/com/example/androiccircle/MainActivity2.kt
@@ -1,21 +1,11 @@
 package com.example.androiccircle
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
-
-    private lateinit var textActivity2: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-
-        textActivity2 = findViewById(R.id.textView1Activity2)
-
-        var contadorDesdeActivity1: Int = intent.getIntExtra("contador", 0)
-
-        textActivity2.text = "Valor de contador es => $contadorDesdeActivity1"
     }
 }
